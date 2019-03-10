0f8a0c308edbc6444a3ea0ae97d83ef46e508bed
Tim C Giles Jr <timothy.giles.12@gmail.com>
2019-03-09 22:23:19 -0500

    add interface and default export

--- src/app/gripes/gripe.ts
+++ src/app/gripes/gripe.ts
@@ -1,4 +1,10 @@
-export class Gripe {
+export interface GripeStruct {
+  $key: string;
+  gripe: string;
+  timeStamp: Date;
+}
+
+export default class Gripe implements GripeStruct {
   $key: string;
   gripe: string;
   timeStamp: Date = new Date();
