454f65321772d9833492230b8d17a80950024cf8
Tim C Giles Jr <timothy.giles.12@gmail.com>
2019-03-09 23:06:44 -0500

    not sure how to store emojis so opReaction is currently string

--- src/app/gripes/gripe.ts
+++ src/app/gripes/gripe.ts
@@ -2,9 +2,11 @@ export interface GripeStruct {
   $key: string;
   gripe: string;
   timeStamp: Date;
+  opReaction: string;
 }
 
 export default class Gripe implements GripeStruct {
+  opReaction: string;
   $key: string;
   gripe: string;
   timeStamp: Date = new Date();
