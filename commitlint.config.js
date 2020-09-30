382b4347c17678dd5792cef1952860e0a2d2c65d
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2020-09-29 21:05:03 -0400

    build: add commit linting

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+      "type-enum": [2, "always", [
+          'build',
+          'feat',
+          'ci',
+          'build',
+          'chore',
+          'fix',
+          'docs',
+          'style',
+          'refactor',
+          'test',
+          'revert',
+          'release'
+      ]
+      ]
+  }
+};
