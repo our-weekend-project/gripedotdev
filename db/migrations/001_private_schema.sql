d6d033533400d86dca782953000b74e38569dde9
Austin T Schaffer <schaffer.austin.t@gmail.com>
2020-10-07 16:25:50 -0400

    Revert "feat: add a private schema for non-public objects"
    This reverts commit 2391420874c75ba385ebf5e009b175dd3f54744c.

--- db/migrations/001_private_schema.sql
+++ db/migrations/001_private_schema.sql
@@ -2,41 +2,41 @@
 
 -- private schema for non-public objects
 create schema "private";
-comment on schema private is 'Private schema for non-public objects that should not be exposed through the GraphQL API.';
 
 -- loads the "pgcrypto" extension in both schemas
 create extension if not exists "pgcrypto" schema "public";
 create extension if not exists "pgcrypto" schema "private";
 
 -- private "login_credentials" table for user login info
-create table private.login_credentials (
+create table "private"."login_credentials" (
     "user_id" integer primary key references public.users("id") on delete cascade,
     "email" text not null unique check (email ~* '^.+@.+\..+$'),
     "password_hash" text not null
 );
-
-create index "login_credentials_email" on private.login_credentials using btree ("email");
-
-comment on table private.login_credentials is 'Holds the email address and password_hash for a user account. 1-to-1 relation to the public.users table.';
+create index "login_credentials_email" on "private"."login_credentials" using btree ("email");
 
 -- Function for creating user accounts
-create function public.register_user(
+create function "public"."register_user"(
     "name" text,
     "email" text,
     "password" text
-) returns public.users as $$
+) returns "public"."users" as $$
 declare
-    "user" public.users;
+  "user" "public"."users";
 begin
-    insert into public.users ("name")
-        values ("name")
-        returning * into "user";
+  insert into "public"."users" ("name") values
+    ("name")
+    returning * into "user";
 
-    insert into private.login_credentials ("user_id", "email", "password_hash")
-        values ("user"."id", "email", crypt("password", gen_salt('bf')));
+  insert into "private"."login_credentials" ("user_id", "email", "password_hash") values
+    ("user"."id", "email", crypt("password", gen_salt('bf')));
 
   return "user";
 end;
 $$ language plpgsql strict security definer;
 
-comment on function public.register_user(text, text, text) is 'Creates a user account in the users table and associates a login_credentials record with the user. Allows the user to log in using an email/password combination.';
+-- TODO:
+--  - restrict post/reaction creation to a logged in user
+--  - add a username column?
+--  - add roles and set default postgraphile role to anonymous role
+--  - https://www.graphile.org/postgraphile/postgresql-schema-design/#postgres-roles
